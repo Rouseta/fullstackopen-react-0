2476e1fa8170312669b1c197d7c021c893f5f554
Rouseta <catacrocken@gmail.com>
2023-09-26 15:43:44 +0200

    add obects instead of variables

--- part1/src/index.js
+++ part1/src/index.js
@@ -31,18 +31,27 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const total = exercises1 + exercises2 + exercises3
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  const total = part1.exercises + part2.exercises + part3.exercises
+
+
 
   return (
     <div>
       <Header title={course} />
-      <Content parts1={part1} parts2={part2} parts3={part3} exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} />
+      <Content parts1={part1.name} parts2={part2.name} parts3={part3.name} exercise1={part1.exercises} exercise2={part2.exercises} exercise3={part3.exercises} />
       <Total total={total} />
     </div >
   )
